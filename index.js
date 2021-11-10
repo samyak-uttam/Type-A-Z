b9773ae207b9cb40babece54a44c0ba40231d06e
samyak-uttam <samyakuttam@gmail.com>
2021-11-10 13:16:52 +0530

    Implementation completed

--- index.js
+++ index.js
@@ -1,13 +1,56 @@
-var alphabets = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '✓'];
-var cur = 0;
+var alphabets = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '✓'];
+var ind = 0, prevTime, stopwatchInterval, elapsedTime = 0;
 
 $(".alphabet-input").keypress(function(event) {
-  if(event.key.toUpperCase() === alphabets[cur]) {
-    $(".next-alphabet").text(alphabets[++cur]);
+  if (event.key.toLowerCase() === alphabets[ind]) {
+    $(".next-alphabet").text(alphabets[++ind].toUpperCase());
+    if (!stopwatchInterval) {
+      stopwatchInterval = setInterval(function() {
+        if (!prevTime)
+          prevTime = Date.now();
+        elapsedTime = Date.now() - prevTime;
+        $(".time").text("Time: " + getTime());
+      }, 50);
+    }
+    if (ind == 26)
+      stopTime();
+    $("." + alphabets[ind - 1]).text(alphabets[ind - 1] + ": " + getTime());
   }
 });
 
 $(".reset-button").click(function() {
-  $(".alphabet-input").val("");
-  $(".next-alphabet").text(alphabets[cur = 0]);
+  reset();
 });
+
+$(document).keypress(function(event) {
+  if (event.which == 13)
+    reset();
+});
+
+function getTime() {
+  var tempTime = elapsedTime;
+  var milliseconds = tempTime % 1000;
+  tempTime = Math.floor(tempTime / 1000);
+  var seconds = tempTime % 60;
+  var zeros = new Array(3 - milliseconds.toString().length + 1).join('0');
+  return seconds + "." + zeros + milliseconds + "s";
+}
+
+function reset() {
+  $(".alphabet-input").val("");
+  $(".next-alphabet").text(alphabets[ind = 0].toUpperCase());
+  stopTime();
+  elapsedTime = 0;
+  $(".time").text("Time: " + getTime());
+  $("li").map(function() {
+    $(this).text($(this).attr("class") + ":");
+  });
+}
+
+function stopTime() {
+  if (stopwatchInterval) {
+    clearInterval(stopwatchInterval);
+    stopwatchInterval = null;
+  }
+  prevTime = null;
+}
